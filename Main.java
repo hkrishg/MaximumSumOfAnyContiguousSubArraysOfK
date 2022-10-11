16a1ca7b60e45829ebabee400e2d6a3197a32b78
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-11 01:01:36 +0000

    Added Problem statement

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
 
+//   Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+
+// Example 1:
+
+// Input: [2, 1, 5, 1, 3, 2], k=3 
+// Output: 9
+// Explanation: Subarray with maximum sum is [5, 1, 3].
+
+//   Example 2:
+
+// Input: [2, 3, 4, 1, 5], k=2 
+// Output: 7
+// Explanation: Subarray with maximum sum is [3, 4].
+
   public static int maximumSumOfAnyContiguousSubArraysOfK(int k, int[] arr) {
     int result = 0;
     int start = 0;
@@ -25,7 +39,6 @@ class Main {
   }
 
   public static void main(String[] args) {
-    // [2, 1, 5, 1, 3, 2], k=3 ------- > output = 9
     int result = Main.maximumSumOfAnyContiguousSubArraysOfK(3, new int[] { 2, 1, 5, 1, 3, 2 });
     System.out.println("----" + result);
   }
